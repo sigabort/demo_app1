ef986dee61ee61ed9d71f6c687a35cd0d6906599
superguy <superguy@super.com>
2010-06-21 22:51:00 -0400

    finishing modeling users part2

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,6 +19,35 @@ describe UsersController do
       get 'new'
       response.should have_tag("title", /Sign up/)
     end
+  end
+
+
+  describe "GET 'show'" do
+    before (:each) do
+      @user = Factory(:user)
+      User.stub!(:find, @user.id).and_return(@user)
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+    
+    it "should contain user's name in title" do
+      get :show, :id => @user
+      response.should have_tag("title", /#{@user.name}/)
+    end
+
+    it "should contain user's name in body" do
+      get :show, :id => @user
+      response.should have_tag("h2", /#{@user.name}/)
+    end
+
+    it "should contain gravatar image" do
+      get :show, :id => @user
+      response.should have_tag("h2>img", :class=>"gravatar")
+    end
     
   end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
+    str = rand_str;
     @attributes = {
-      :name => rand_str,
-      :email => rand_str
+      :name => str,
+      :email => str,
+      :password => str,
+      :password_confirmation => str
     }
   end
 
@@ -32,6 +35,70 @@ describe User do
     User.create!(@attributes)
     dup_user = User.new(@attributes.merge(:name => rand_str()))
     dup_user.should_not be_valid
+  end
+  
+  #password validations
+  describe "Password validations" do
+    before(:each) do
+      @user = User.create!(@attributes)
+    end
+    
+    it "shoud require a password" do
+      no_passwd_user = User.new(@attributes.merge(:password=>""))
+      no_passwd_user.should_not be_valid
+    end
+    
+    it "shoud confirm a password" do
+      User.new(@attributes.merge(:password_confirmation=>rand_str)).
+      should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      User.new(@attributes.merge(:password => 'a'*5)).
+      should_not be_valid
+    end
+    
+    it "should reject long passwords" do
+      User.new(@attributes.merge(:password => 'a'*50)).
+      should_not be_valid
+    end
+    
+    it "should have an encrypted password" do
+      @user.should respond_to("encrypted_password") 
+    end
+    
+    it "encrypted password should not be blank" do
+      @user.encrypted_password.should_not be_blank
+    end
+
+    describe "has_password method" do
+      it "should be true if the passwords match" do
+        @user.has_password?(@attributes[:password]).should be_true
+      end
+      
+      it "should be false if the passwords dont match" do
+        @user.has_password?(rand_str).should be_false
+      end
+    end
+
+    describe "authenticate method" do
+      it "should return nil when the password/email dont match" do
+        user_password_no_match = User.authenticate(@attributes[:email], rand_str)
+        user_password_no_match.should be_nil
+      end
+      
+      it "should return user when password/email match" do
+        user_match = User.authenticate(@attributes[:email], @attributes[:password])
+        user_match.should == @user
+      end
+      
+      it "should return nil when user with email doesn't exist" do
+        user_match = User.authenticate(rand_str, @attributes[:password])
+        user_match.should be_nil        
+      end
+      
+    end
+
   end
   
 end
